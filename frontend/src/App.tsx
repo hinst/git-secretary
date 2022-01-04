9138f558382ca7f87b55abf80119bd608812460b
hinst <hinstance@yandex.ru>
2022-01-05 00:18:54 +0100

    Re-generated React App from template

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,26 @@
-import React, { ChangeEvent, Component } from 'react';
-import './w3.css';
-import './git-stories.css'
-import { RepoHistoryViewer } from './RepoHistoryViewer';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-    render() {
-        document.title = 'Git Stories';
-        return <RepoHistoryViewer/>;
-    }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
