ff993bb0e2f0f9c1f72b303ad72936b913d62794
hinst <hinstance@yandex.ru>
2022-01-07 00:28:03 +0100

    I am removing go-api-file-picker submodule because I do not want to keep it in a separated repo from the very beginning.

---
+++ git-stories-server/FilePicker.go
@@ -0,0 +1,29 @@
+package go_api_file_picker
+
+import (
+	"encoding/json"
+	"io/fs"
+	"net/http"
+	"path/filepath"
+
+	"github.com/hinst/go-common"
+)
+
+type FilePicker struct {
+}
+
+type FileInfo struct {
+	name        string
+	isDirectory bool
+}
+
+func (picker *FilePicker) GetFileList(responseWriter http.ResponseWriter, request *http.Request) {
+	var directory = request.URL.Query().Get("directory")
+	var fileInfos []FileInfo
+	filepath.Walk(directory, func(path string, info fs.FileInfo, err error) error {
+		fileInfos = append(fileInfos, FileInfo{name: path, isDirectory: info.IsDir()})
+		return nil
+	})
+	var data, error = json.Marshal(fileInfos)
+	common.AssertError()
+}
