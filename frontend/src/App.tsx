902f1210256c3df96614ee603d5dc98afa8e8c49
hinst <hinstance@yandex.ru>
2022-01-12 00:17:37 +0100

    Added route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,16 @@ import { RepoHistoryViewer } from './RepoHistoryViewer';
 import './App.css';
 import './git-stories.css';
 import './external/w3.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   document.title = 'Git Stories';
-  return <RepoHistoryViewer/>;
+  return (<BrowserRouter>
+    <Routes>
+      <Route path="/" element={<RepoHistoryViewer/>}>
+      </Route>
+    </Routes>
+  </BrowserRouter>);
 }
 
 export default App;
