2fda2df2f8ae7cb5aca23e4faf8610c56901b593
hinst <hinstance@yandex.ru>
2022-01-12 21:53:21 +0100

    Removed old CSS file: git-stories.css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { RepoHistoryViewer } from './RepoHistoryViewer';
 import './App.css';
-import './git-stories.css';
 import './external/w3.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Common } from './Common';
