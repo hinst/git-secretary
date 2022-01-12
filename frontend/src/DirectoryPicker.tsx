508d94affe8ea6b46d6bceeb277ffa50b682dbb3
hinst <hinstance@yandex.ru>
2022-01-12 01:30:35 +0100

    Filling in code for DirectoryPicker (draft)

--- frontend/src/DirectoryPicker.tsx
+++ frontend/src/DirectoryPicker.tsx
@@ -1,16 +1,41 @@
-import { Component, ReactNode } from "react";
+import { Component } from "react";
+import { Common } from "./Common";
 
 class Props {
 }
 
+interface FileInfo {
+    name: string;
+    path: string;
+    isDirectory: boolean;
+}
+
 class State {
-    currentDirectory?: string = undefined;
+    currentDirectory: string = '';
+    files: FileInfo[] = [];
 }
 
 export class DirectoryPicker extends Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = new State();
+    }
+
+    componentDidMount() {
+        this.loadFileList();
+    }
+
     override render() {
-        return <div className="w3-panel">
-            <div className="w3-bar-item">Choose a directory containing a Git repository</div>
+        return <div className="w3-bar w3-border-bottom" style={{position: 'sticky', top: 0}}>
+            <div className="w3-bar-item">Please pick directory containing a Git repository</div>
         </div>;
+    }
+
+    private async loadFileList() {
+        const url = Common.apiUrl + '/fileList?directory=' +
+            encodeURIComponent(this.state.currentDirectory);
+        const response = await fetch(url);
+        const files = await response.json();
+        this.setState({files: files});
     }
 }
