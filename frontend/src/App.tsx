ca8e5f8f3673300731187fdd51b09b544fd7a9ef
hinst <hinstance@yandex.ru>
2022-01-12 22:28:11 +0100

    App: added sticky header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,12 +7,18 @@ import { DirectoryPicker } from './DirectoryPicker';
 
 function App() {
     document.title = 'Git Stories';
-    return <BrowserRouter>
-        <Routes>
-            <Route path={Common.baseUrl + '/open-repository'} element={<DirectoryPicker/>} />
-            <Route path={Common.baseUrl} element={<RepoHistoryViewer/>} />
-        </Routes>
-    </BrowserRouter>;
+    const directoryPicker = <DirectoryPicker setDirectory={(directory) => console.log(directory)}/>;
+    return <div style={{margin: 4}}>
+        <BrowserRouter>
+            <div className="w3-bar w3-dark-grey" style={{marginBottom: 4, position: 'sticky', top: 0}}>
+                <a href={Common.baseUrl + '/'} className="w3-bar-item w3-black w3-btn">GIT-STORIES</a>
+            </div>
+            <Routes>
+                <Route path={Common.baseUrl + '/open-repository'} element={directoryPicker} />
+                <Route path={Common.baseUrl} element={<RepoHistoryViewer/>} />
+            </Routes>
+        </BrowserRouter>
+    </div>;
 }
 
 export default App;
