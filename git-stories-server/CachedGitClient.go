7bd553c746836ffba874e8eeb82e5bd7d686622c
hinst <hinstance@yandex.ru>
2022-01-24 22:24:02 +0100

    Refactored -> Added cachedGitClient_DetailedLogReader

--- git-stories-server/CachedGitClient.go
+++ git-stories-server/CachedGitClient.go
@@ -26,59 +26,82 @@ func (client *CachedGitClient) ReadDetailedLog(lengthLimit int) ([]git_stories_a
 	if nil != readError {
 		return nil, readError
 	}
-	var rows []git_stories_api.DetailedLogEntryRow
-	var newRows []git_stories_api.DetailedLogEntryRow
-	var transactionError = client.storage.View(func(transaction *bolt.Tx) error {
-		var bucket, bucketError = transaction.CreateBucketIfNotExists(BUCKET_NAME_LOG_ENTRY_ROWS_BYTES)
+	var logReader cachedGitClient_DetailedLogReader
+	logReader.Create(client.storage, client.gitClient)
+	var error = logReader.Load(logEntries)
+	return logReader.GetRows(), error
+}
+
+type cachedGitClient_DetailedLogReader struct {
+	storage    *bolt.DB
+	gitClient  *GitClient
+	logEntries []LogEntryRow
+	rows       []git_stories_api.DetailedLogEntryRow
+	newRows    []git_stories_api.DetailedLogEntryRow
+}
+
+func (reader *cachedGitClient_DetailedLogReader) Create(storage *bolt.DB, gitClient *GitClient) *cachedGitClient_DetailedLogReader {
+	reader.storage = storage
+	reader.gitClient = gitClient
+	return reader
+}
+
+func (reader *cachedGitClient_DetailedLogReader) Load(logEntries []LogEntryRow) error {
+	reader.logEntries = logEntries
+	var transactionError = reader.storage.View(reader.loadRows)
+	if nil != transactionError {
+		return transactionError
+	}
+	if len(reader.newRows) > 0 {
+		transactionError = reader.storage.Update(reader.storeCachedRows)
+	}
+	return transactionError
+}
+
+func (reader *cachedGitClient_DetailedLogReader) loadRows(transaction *bolt.Tx) error {
+	var bucket, bucketError = transaction.CreateBucketIfNotExists(BUCKET_NAME_LOG_ENTRY_ROWS_BYTES)
+	if nil != bucketError {
+		return bucketError
+	}
+	for _, entry := range reader.logEntries {
+		var row git_stories_api.DetailedLogEntryRow
+		var cachedRowBytes = bucket.Get([]byte(entry.CommitHash))
+		if cachedRowBytes == nil { // new row
+			var e error
+			row, e = reader.gitClient.ReadDetailedLogEntryRow(entry)
+			if nil != e {
+				return e
+			}
+			reader.newRows = append(reader.newRows, row)
+		} else { // cached row
+			var jsonError = json.Unmarshal(cachedRowBytes, &row)
+			if nil != jsonError {
+				return nil
+			}
+		}
+		reader.rows = append(reader.rows, row)
+	}
+	return nil
+}
+
+func (reader *cachedGitClient_DetailedLogReader) storeCachedRows(transaction *bolt.Tx) error {
+	var bucket, bucketError = transaction.CreateBucketIfNotExists(BUCKET_NAME_LOG_ENTRY_ROWS_BYTES)
+	if nil != bucketError {
+		return bucketError
+	}
+	for _, row := range reader.newRows {
+		var rowBytes, jsonError = json.Marshal(row)
+		if nil != jsonError {
+			return jsonError
+		}
+		bucketError = bucket.Put([]byte(row.CommitHash), rowBytes)
 		if nil != bucketError {
 			return bucketError
 		}
-		for _, entry := range logEntries {
-			var row git_stories_api.DetailedLogEntryRow
-			var cachedRowBytes = bucket.Get([]byte(entry.CommitHash))
-			if cachedRowBytes == nil { // new row
-				var e error
-				row, e = client.gitClient.ReadDetailedLogEntryRow(entry)
-				if nil != e {
-					return e
-				}
-				newRows = append(newRows, row)
-			} else { // cached row
-				var jsonError = json.Unmarshal(cachedRowBytes, &row)
-				if nil != jsonError {
-					return nil
-				}
-			}
-			rows = append(rows, row)
-		}
-		return nil
-	})
-	if len(newRows) > 0 { // store newly loaded
-		client.storage.Update(func(transaction *bolt.Tx) error {
-			var bucket, bucketError = transaction.CreateBucketIfNotExists(BUCKET_NAME_LOG_ENTRY_ROWS_BYTES)
-			if nil != bucketError {
-				return bucketError
-			}
-			for _, row := range newRows {
-				var rowBytes, jsonError = json.Marshal(row)
-				if nil != jsonError {
-					return jsonError
-				}
-				bucketError = bucket.Put([]byte(row.CommitHash), rowBytes)
-				if nil != bucketError {
-					return bucketError
-				}
-			}
-			return nil
-		})
 	}
-	if nil != transactionError {
-		return nil, transactionError
-	}
-	return rows, nil
+	return nil
 }
 
-func (client *CachedGitClient) ReadDetailedLogRow(entry LogEntryRow, cachedRowBytes []byte) (
-	row git_stories_api.DetailedLogEntryRow) {
-
+func (reader *cachedGitClient_DetailedLogReader) GetRows() []git_stories_api.DetailedLogEntryRow {
+	return reader.rows
 }
