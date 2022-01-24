5258613aa874259654aebf4080520abd5b9082b6
hinst <hinstance@yandex.ru>
2022-01-24 22:47:23 +0100

    Added function Configuration.SetDefault()

--- git-stories-server/configuration.go
+++ git-stories-server/configuration.go
@@ -4,7 +4,15 @@ import "runtime"
 
 type Configuration struct {
 	Plugin     string
+	WebPath    string
 	PortNumber int
+}
+
+func (configuration *Configuration) SetDefault() *Configuration {
+	configuration.Plugin = "plugins/story-girls-standard"
+	configuration.WebPath = "/git-stories"
+	configuration.PortNumber = 3003
+	return configuration
 }
 
 const WINDOWS_EXECUTABLE_FILE_EXTENSION = ".exe"
