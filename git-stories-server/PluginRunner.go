768cbf291eba169c351bcdf3b2f1da6af388008e
hinst <hinstance@yandex.ru>
2022-01-25 22:52:55 +0100

    Added true error handling in PluginRunner.Run

--- git-stories-server/PluginRunner.go
+++ git-stories-server/PluginRunner.go
@@ -15,27 +15,49 @@ type PluginRunner struct {
 	PluginFilePath string
 }
 
-func (runner *PluginRunner) Run(rows git_stories_api.DetailedLogEntryRows) []git_stories_api.StoryEntry {
+func (runner *PluginRunner) Run(rows git_stories_api.DetailedLogEntryRows) ([]git_stories_api.StoryEntry, error) {
 	var rowsBytes, jsonWriteError = json.Marshal(rows)
-	common.AssertError(jsonWriteError)
+	if nil != jsonWriteError {
+		return nil, common.CreateException("Cannot to write json", jsonWriteError)
+	}
 	var pluginFilePath = runner.PluginFilePath
 	if CheckWindows() && !strings.HasSuffix(pluginFilePath, WINDOWS_EXECUTABLE_FILE_EXTENSION) {
 		pluginFilePath += WINDOWS_EXECUTABLE_FILE_EXTENSION
 	}
 	var command = exec.Command(pluginFilePath)
 	var writer, writerError = command.StdinPipe()
-	common.AssertError(writerError)
+	if nil != writerError {
+		return nil, common.CreateException("Cannot write input for the plug-in", writerError)
+	}
 	var output, pluginOutputError = command.StdoutPipe()
-	common.AssertError(pluginOutputError)
-	common.AssertError(command.Start())
+	if nil != pluginOutputError {
+		return nil, common.CreateException("Cannot open output of the plug-in", pluginOutputError)
+	}
+	var commandStartError = command.Start()
+	if nil != commandStartError {
+		return nil, common.CreateException("Cannot start command", commandStartError)
+	}
 	var bufferedWriter = bufio.NewWriter(writer)
 	var _, bufferedWriteError = bufferedWriter.Write(rowsBytes)
-	common.AssertError(bufferedWriteError, bufferedWriter.Flush())
+	if nil != bufferedWriteError {
+		return nil, common.CreateException("Cannot write buffered", bufferedWriteError)
+	}
+	var flushError = bufferedWriter.Flush()
+	if nil != flushError {
+		return nil, common.CreateException("Cannot flush writer", flushError)
+	}
 	var closeError = writer.Close()
-	common.AssertError(closeError)
+	if nil != closeError {
+		return nil, common.CreateException("Cannot close writer", closeError)
+	}
 	var outputData, outputError = ioutil.ReadAll(bufio.NewReader(output))
-	common.AssertError(outputError)
+	if nil != outputError {
+		return nil, common.CreateException("Cannot read output of the plug-in", outputError)
+	}
 	var storyEntries []git_stories_api.StoryEntry
-	json.Unmarshal(outputData, &storyEntries)
-	return storyEntries
+	var jsonError = json.Unmarshal(outputData, &storyEntries)
+	if nil != jsonError {
+		return nil, common.CreateException("Cannot decode output of the plug-in as JSON", jsonError)
+	}
+	return storyEntries, nil
 }
