12c5bb788b530142fc690f8b885dd3e994f58c01
hinst <hinstance@yandex.ru>
2022-02-24 22:40:15 +0100

    Build package for windows & linux

--- build-package.ps1
+++ build-package.ps1
@@ -1,1 +1,23 @@
+if (Test-Path .\dist) {
+    Remove-Item -Recurse .\dist
+}
+mkdir .\dist
+
 $Null = &".\build.ps1"
+
+Copy-Item -Recurse -Path .\frontend\build -Destination .\dist\frontend\
+Copy-Item configuration.json .\dist\configuration.json
+
+if (Test-Path .\git-secretary-windows) {
+    Remove-Item -Recurse .\git-secretary-windows
+}
+Copy-Item -Recurse -Path .\dist .\git-secretary-windows
+Copy-Item .\server\git-secretary.exe .\git-secretary-windows\
+Compress-Archive -Path .\git-secretary-windows -DestinationPath .\git-secretary-windows.zip -Force
+
+if (Test-Path .\dist-linux) {
+    Remove-Item -Recurse .\dist-linux
+}
+Copy-Item -Recurse -Path .\dist .\git-secretary-linux
+Copy-Item .\server\git-secretary .\git-secretary-linux\
+Compress-Archive -Path .\git-secretary-linux -DestinationPath .\git-secretary-linux.zip -Force
